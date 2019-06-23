d6c11379148c6daa527eeb5bfce1484951ae9bfe
filletzz <filletzz@gmail.com>
2019-06-23 13:28:22 +0200

    Initial commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Petr Chmelar on 23/06/2019.
+//  Copyright © 2019 Qest. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello SwiftUI")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
